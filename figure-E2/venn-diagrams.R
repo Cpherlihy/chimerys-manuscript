e467a5a1ae722d84dba57b68f0abc2fdfde38f4d
Alexander Hogrebe <alexander.hogrebe@msaid.de>
2024-12-17 15:28:22 +0100

    8b902ed fst to csv

--- figure-E2/venn-diagrams.R
+++ figure-E2/venn-diagrams.R
@@ -23,27 +23,27 @@ data_pcm_ptm <- data_pcm[condition %in% c("MSFragger", "MSFragger-DDAplus"),
 psm_se <- c("MSF-200", "MS-GF-plus", "MaxQuant", "Metamorpheus", "MS-Amanda", "Comet", "Sequest-HT")
 #data_psm[condition %in% psm_se, .N, keyby = c("condition")]
 data_venn_psm_duell <-
-  rbind(data_psm[condition %in% psm_se, .(condition = factor("7 other\nsearch\nengines"), .N), by = c("psm_J")],
+  rbind(data_psm[condition %in% psm_se, .(condition = factor("7 other search engines"), .N), by = c("psm_J")],
         data_psm[condition %in% "CHIMERYS", .(condition = factor("CHIMERYS"), .N), by = c("psm_J")])
-write_fst(data_venn_psm_duell, file.path(figurePath, "data_venn_psm_duell.fst"))
+fwrite(data_venn_psm_duell, file.path(figurePath, "figure-E2A-venn-psms.csv"), quote = T)
 
 
 #PTM group level - native only ====
 ptm_native_se <- c("MaxQuant", "Comet", "SequestHT", "MSAmanda")
 #data_ptm[condition %in% ptm_native_se, .N, keyby = c("condition")]
 data_venn_ptm_native_duell <-
-  rbind(data_psm[condition %in% ptm_native_se, .(condition = factor("4 other\nsearch\nengines"), .N), by = c("ptm_group_J")],
+  rbind(data_psm[condition %in% ptm_native_se, .(condition = factor("4 other search engines"), .N), by = c("ptm_group_J")],
         data_ptm[condition %in% "CHIMERYS", .(condition = factor("CHIMERYS"), .N), by = c("ptm_group_J")])
-write_fst(data_venn_ptm_native_duell, file.path(figurePath, "data_venn_ptm_native_duell.fst"))
+fwrite(data_venn_ptm_native_duell, file.path(figurePath, "figure-E2B-venn-peptides-native.csv"), quote = T)
 
 
 #PTM group level - all ====
 ptm_native_se <- c("MaxQuant", "Comet", "SequestHT", "MSAmanda")
 data_ptm_joined <-
-  rbind(data_ptm[condition %in% ptm_native_se, .(condition = factor("7 other\nsearch\nengines"), .N), by = c("ptm_group_J")],
-        data_pcm_ptm[condition %in% "MSF-200", .(condition = factor("7 other\nsearch\nengines"), .N), by = c("ptm_group_J")],
+  rbind(data_ptm[condition %in% ptm_native_se, .(condition = factor("7 other search engines"), .N), by = c("ptm_group_J")],
+        data_pcm_ptm[condition %in% "MSF-200", .(condition = factor("7 other search engines"), .N), by = c("ptm_group_J")],
         data_psm_ptm[condition %in% c("Metamorpheus", "MS-GF-plus"),
-                     .(condition = factor("7 other\nsearch\nengines"), .N), by = c("ptm_group_J")])
+                     .(condition = factor("7 other search engines"), .N), by = c("ptm_group_J")])
 data_venn_ptm_all_duell <- rbind(data_ptm_joined,
                                  data_ptm[condition %in% "CHIMERYS", .(condition = factor("CHIMERYS"), .N), by = c("ptm_group_J")])
-write_fst(data_venn_ptm_all_duell, file.path(figurePath, "data_venn_ptm_all_duell.fst"))
+fwrite(data_venn_ptm_all_duell, file.path(figurePath, "figure-E2C-venn-peptides-all.csv"))
