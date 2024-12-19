0cfeb3665d06816e1e9d4027311f065240c3e6c7
Alexander Hogrebe <alexander.hogrebe@msaid.de>
2024-12-19 17:09:57 +0100

    a34fcf5 figure E10

---
+++ figure-E10/README.md
@@ -0,0 +1,113 @@
+# Figure E10
+MSAID
+2024-12-19
+
+- [Overview](#overview)
+- [Data](#data)
+  - [Peptide length distributions](#peptide-length-distributions)
+  - [Counts](#counts)
+- [Figure](#figure)
+
+# Overview
+
+This document describes how the data analysis and plots for extended
+figure 10 were generated. To recreate the figures, make sure to download
+all input files (available on
+[PRIDE](https://www.ebi.ac.uk/pride/archive?keyword=PXD053241)), place
+them under `dataPath` (adjust in `load-dependencies.R` to your own
+folder structure) and generate intermediate results in the linked `.R`
+scripts.
+
+<details>
+<summary>
+Details on data processing
+</summary>
+
+``` r
+suppressMessages(source(here::here("scripts/load-dependencies.R")))
+path <- file.path(here::here(), "figure-E10")
+figurePath <- file.path(dataPath, "data/figure-E10")
+msaid_td <- c("Target" = msaid_blue, "Decoy" = msaid_orange)
+```
+
+</details>
+
+# Data
+
+<details>
+<summary>
+Details on data loading
+</summary>
+
+[R code to generate `.csv` input
+files](figure-E10-library-and-csodiaq-results.R)
+
+## Peptide length distributions
+
+``` r
+pep_zodiaq <- fread(file.path(figurePath, "figure-E10A-csodiaq.csv"))
+pep_zodiaq[, isDecoyLabel := factor(isDecoyLabel, c("Target", "Decoy"))]
+
+p_pep_zodiaq <- ggplot(pep_zodiaq, aes(x=n_aa, fill=isDecoyLabel)) +
+  geom_bar(position = "identity", alpha = 0.5) +
+  scale_y_continuous(labels = label_number(scale_cut = append(cut_short_scale(), 1, 1))) +
+  scale_fill_manual(NULL, values = msaid_td) +
+  xlab("Peptide length") + ylab("csoDIAq original precursors") +
+  theme(legend.position = "top")
+
+pep_inferys <- fread(file.path(figurePath, "figure-E10B-chimerys.csv"))
+pep_inferys[, isDecoyLabel := factor(isDecoyLabel, c("Target", "Decoy"))]
+
+p_pep_inferys <- ggplot(pep_inferys, aes(x=n_aa, fill=isDecoyLabel)) +
+  geom_bar(position = "identity", alpha = 0.5) +
+  scale_y_continuous(labels = label_number(scale_cut = append(cut_short_scale(), 1, 1))) +
+  scale_fill_manual(NULL, values = msaid_td) +
+  xlab("Peptide length") + ylab("CHIMERYS digest precursors") +
+  theme(legend.position = "top")
+```
+
+## Counts
+
+``` r
+dt <- fread(file.path(figurePath, "figure-E10C-counts.csv"))
+types <- c("CsoDIAq original",
+           "CsoDIAq overlap",
+           "CsoDIAq overlap\nTargets INFERYS",
+           "CsoDIAq overlap\nDecoys INFERYS",
+           "CsoDIAq overlap\nBoth INFERYS",
+           "CHIMERYS digest\nBoth INFERYS")
+dt[, type := factor(type, types)]
+
+p_counts <- ggplot(dt, aes(x=type, y=N)) +
+  geom_boxplot(outliers = F, color = msaid_darkgray) +
+  geom_point(shape = 1L, alpha = 0.5, color = msaid_darkgray,
+             position = position_jitter(0.1, seed = 123L)) +
+  scale_y_continuous(labels = label_number(scale_cut = append(cut_short_scale(), 1, 1))) +
+  xlab(NULL) + ylab("Peptide groups (run-specific)")
+```
+
+</details>
+
+# Figure
+
+<details>
+<summary>
+Details on figure generation
+</summary>
+
+``` r
+p_design <- "AAAAAA\nBBBCCC\nDDDDDD"
+
+p_library <- guide_area() + p_pep_zodiaq + p_pep_inferys + p_counts +
+  plot_layout(heights = c(0.1, 1, 1), design = p_design, guides = "collect") +
+  plot_annotation(tag_levels = list(c("A", "B", "C")))
+
+suppressWarnings(ggsave2(file.path(path, "figure-E10.pdf"), plot = p_library,
+                         width = 180, height = 90, units = "mm", device = cairo_pdf))
+suppressWarnings(ggsave2(file.path(path, "figure-E10.png"), plot = p_library,
+                         width = 180, height = 90, units = "mm"))
+```
+
+</details>
+
+![figure-E10](figure-E10.png)
---
+++ figure-E10/figure-E10-library-and-csodiaq-results.R
@@ -0,0 +1,159 @@
+#setup
+source(here::here("scripts/load-dependencies.R"))
+path <- file.path(here::here(), "figure-E10")
+figurePath <- file.path(dataPath, "data/figure-E10")
+
+nPtm <- function(sequence, ptm = "+15.9949") {
+  nSeq <- nchar(sequence)
+  nSeqNoPtm <- nchar(gsub(ptm, "", sequence, fixed = T))
+  as.integer((nSeq - nSeqNoPtm) / nchar(ptm))
+}
+
+read_zodiaq <- function(data_path, unimods, sample_names_path) {
+  stopifnot(all(c("1", "4", "5", "35") %in% names(unimods)))
+  filePaths <- list.files(data_path, "_fullOutput\\.csv$", full.names = T)
+  zodiaq <- foreach(filePath = filePaths, .combine = rbind) %do% {
+    #print(paste("Loading", basename(filePath)))
+    tmp <- fread(filePath, fill=T)
+    tmp[, file := tools::file_path_sans_ext(basename(fileName))]
+    tmp[, is_decoy := as.logical(isDecoy)]
+    tmp[, MaCC_Score := cosine * shared**0.2]
+    #create peptide groups
+    #unimod 5 is likely misannotated in library, see section above, count as Ac
+    tmp[, peptide := gsub(paste0("C", unimods["4"]), "C", peptide, fixed = T)]
+    tmp[, nAc := nPtm(peptide, unimods["1"])]
+    tmp[, nCa := nPtm(peptide, unimods["5"])]
+    tmp[, nOx := nPtm(peptide, unimods["35"])]
+    tmp[, peptide := gsub(unimods["1"], "", peptide, fixed = T)]
+    tmp[, peptide := gsub(unimods["5"], "", peptide, fixed = T)]
+    tmp[, peptide := gsub(unimods["35"], "", peptide, fixed = T)]
+    tmp[, ptm_group := paste0(peptide, "_", nAc, "x1_", nCa, "x5_0x21_",
+                              nOx, "x35_0x765_0x766")]
+    #collapse onto ptm_group level
+    tmp <- tmp[, .(score = max(MaCC_Score), peptide = peptide[1]),
+               by=.(file, ptm_group, is_decoy)]
+    return(tmp)
+  }
+  setkey(zodiaq, file)
+  #map sample names
+  dtSamples <- readSampleNames(sample_names_path)
+  dtSamples <- dtSamples[, .(file = raw_file, sample = sample_name, condition = condition_name)]
+  setkey(dtSamples, file)
+  zodiaq <- dtSamples[zodiaq]
+
+  #calculate file-local FDR
+  setorder(zodiaq, sample, -score)
+  zodiaq[, q_value := cumsum(is_decoy) / cumsum(!is_decoy), by=sample]
+  setorder(zodiaq, sample, score)
+  zodiaq[, q_value := cummin(q_value), by = sample]
+  zodiaq[is_decoy==FALSE & q_value<=0.01, .N]
+  zodiaq[is_decoy==FALSE & q_value<=0.01, uniqueN(ptm_group)]
+
+  #calculate dataset-global FDR (via best global candidate)
+  fdr_global <- zodiaq[, .(score = max(score)), by=.(ptm_group, is_decoy)]
+  setorder(fdr_global, -score)
+  fdr_global[, q_value_global := cumsum(is_decoy) / cumsum(!is_decoy)]
+  setorder(fdr_global, score)
+  fdr_global[, q_value_global := cummin(q_value_global)]
+  fdr_global[, c("is_decoy", "score") := NULL]
+  setkey(fdr_global, ptm_group)
+  setkey(zodiaq, ptm_group)
+  zodiaq <- fdr_global[zodiaq]
+  setkey(zodiaq, sample, ptm_group)
+  zodiaq[is_decoy==FALSE & q_value_global<=0.01, .N]
+  zodiaq[is_decoy==FALSE & q_value_global<=0.01, uniqueN(ptm_group)]
+
+  #define conditions
+  zodiaq[, replicate := as.integer(gsub("^.*-.*-.*-.*-.*-.*_(.)$", "\\1", sample))]
+  zodiaq[, condition_faims := factor(gsub("^(.*)-.*-.*-.*-.*-.*$", "\\1", condition),
+                                     c("faims", "noFaims"))]
+  zodiaq[, condition_study := factor(gsub("^.*-(.*)-.*-.*-.*-.*$", "\\1", condition),
+                                     c("res", "it", "iw", "ov"))]
+  zodiaq[, condition_resolution := as.integer(gsub("^.*-.*-(.*)k-.*-.*-.*$", "\\1", condition))]
+  zodiaq[, condition_it := as.integer(gsub("^.*-.*-.*-(.*)ms-.*-.*$", "\\1", condition))]
+  zodiaq[, condition_iw := as.integer(gsub("^.*-.*-.*-.*-(.*)th-.*$", "\\1", condition))]
+  zodiaq[, condition_overlap := factor(gsub("^.*-.*-.*-.*-.*-(.*)$", "\\1", condition),
+                                       c("0.5ov", "1ov", "3ov", "4ov", "6ov", "8ov"))]
+  return(zodiaq[])
+}
+
+#Load libraries for peptide length distributions
+lib_zodiaq <- fread(file.path(dataPath, "Direct_infusion/human_lib.tsv")) #3108165
+#fix decoy column (UniprotID is sorted, if first entry target then not a decoy)
+lib_zodiaq[, isDecoy := grepl("^\\d{1,2}/DECOY", UniprotID)]
+lib_zodiaq[, isDecoyLabel := factor(ifelse(isDecoy, "Decoy", "Target"), c("Target", "Decoy"))]
+pep_zodiaq <- lib_zodiaq[, .(n_aa = nchar(PeptideSequence[1])),
+                         by=.(isDecoyLabel, PeptideGroupLabel)] #273373
+fwrite(pep_zodiaq, file.path(figurePath, "figure-E10A-csodiaq.csv"))
+
+lib_inferys <- fread(file.path(dataPath, "Direct_infusion/human_lib_inferys_filtered.tsv")) #2599104
+lib_inferys[, isDecoyLabel := factor(ifelse(isDecoy, "Decoy", "Target"), c("Target", "Decoy"))]
+pep_inferys <- lib_inferys[, .(n_aa = nchar(PeptideSequence[1])),
+                           by=.(isDecoyLabel, PeptideGroupLabel)] #259916
+fwrite(pep_inferys, file.path(figurePath, "figure-E10B-chimerys.csv"))
+
+
+#Load search engine counts
+search_results_folder <- file.path(dataPath, "Direct_infusion/CsoDIAq")
+#search_results_folder <- "/mnt/storage1/0_Users/Alex/Projects/direct-infusion/2024-09_i81_Meyer-2020-DISPA"
+
+#complete csoDIAq
+unimods <- c("1" = "(UniMod:1)", "4" = "(UniMod:4)", "5" = "(UniMod:5)", "35" = "(UniMod:35)")
+study_path <- "ZO_2024-09-27_zodiaq-mcf7-tsv-library"
+zodiaq_original <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+#Filtered to overlap
+unimods <- c("1" = "(UniMod:1)", "4" = "(UniMod:4)", "5" = "(UniMod:5)", "35" = "(UniMod:35)")
+study_path <- "ZO_2024-10-14_zodiaq-mcf7-tsv-library-filtered-to-inferys"
+zodiaq_overlap <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+#Targets INFERYS
+unimods <- c("1" = "[UNIMOD:1]", "4" = "[UNIMOD:4]", "5" = "[UNIMOD:5]", "35" = "[UNIMOD:35]")
+study_path <- "ZO_2024-10-17_inferys_targets_zodiaq_decoys_noNL_morethan400mz"
+zodiaq_T_inferys <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+#Decoys INFERYS
+unimods <- c("1" = "[UNIMOD:1]", "4" = "[UNIMOD:4]", "5" = "[UNIMOD:5]", "35" = "[UNIMOD:35]")
+study_path <- "ZO_2024-10-17_zodiaq_targets_inferys_decoys_noNL_morethan400mz"
+zodiaq_D_inferys <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+#Both INFERYS
+unimods <- c("1" = "[UNIMOD:1]", "4" = "[UNIMOD:4]", "5" = "[UNIMOD:5]", "35" = "[UNIMOD:35]")
+study_path <- "ZO_2024-10-17_inferys_targets_inferys_decoys_noNL_morethan400mz"
+zodiaq_both_inferys <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+#CHIMERYS digest
+unimods <- c("1" = "[UNIMOD:1]", "4" = "[UNIMOD:4]", "5" = "[UNIMOD:5]", "35" = "[UNIMOD:35]")
+study_path <- "ZO_2024-10-17_inferys_filtered_noNL_morethan400mz"
+chimerys_digest <-
+  read_zodiaq(data_path = file.path(search_results_folder, study_path),
+              unimods = unimods, sample_names_path = file.path(path, "sample_names.csv"))
+
+types <- c("CsoDIAq original",
+           "CsoDIAq overlap",
+           "CsoDIAq overlap\nTargets INFERYS",
+           "CsoDIAq overlap\nDecoys INFERYS",
+           "CsoDIAq overlap\nBoth INFERYS",
+           "CHIMERYS digest\nBoth INFERYS")
+results <-
+  rbind(cbind(type = types[1L], zodiaq_original),
+        cbind(type = types[2L], zodiaq_overlap),
+        cbind(type = types[3L], zodiaq_T_inferys),
+        cbind(type = types[4L], zodiaq_D_inferys),
+        cbind(type = types[5L], zodiaq_both_inferys),
+        cbind(type = types[6L], chimerys_digest))
+results[, type := factor(type, types)]
+results_local <- results[is_decoy==FALSE & q_value<=0.01]
+count_local <- results_local[, .N, keyby=.(type, sample)]
+
+fwrite(count_local, file.path(figurePath, "figure-E10C-counts.csv"))
